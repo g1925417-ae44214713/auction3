345a992f21eda8a17a92d374d1328e04d345ff86
g1925417 <g1925417@yahoo.co.jp>
2020-03-09 00:03:46 +0900

    add 詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  # get '/items' => 'items#index'
+  # post '/items' => 'items#create'
+  # get '/items/new' => 'items#new'   # newとshowの順番に気をつけて下さい
+  # get '/items/:id' => 'items#show'  # 逆に書いてしまうと、全てshowで処理されます
+  # get '/items/:id/edit' => 'items#edit'
+  # patch '/items/:id' => 'items#update'
+  # delete '/items/:id' => 'items#destroy'
+
+  resources :items
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
